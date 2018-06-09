e11f906c6a5baa02617717c327fcfff1c680eb0b
sk <dev.seakim@gmail.com>
2018-06-09 00:37:19 -0400

    added RCB-hw4 on portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,3 @@
+$(document).ready(function () {
 
-// ftr_btn function to pause and play
-var video = document.getElementById("ftr_video");
-var btn = document.getElementById("ftr_btn");
-function play_pause() {
-    if (video.paused) {
-        video.play();
-        btn.innerHTML = "Pause";
-    } else {
-        video.pause();
-        btn.innerHTML = "Resume";
-    }
-}
+});
