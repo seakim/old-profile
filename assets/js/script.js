eb9fa520b1875e212e7a0d7b0adcdbd6e4e212b0
sk <dev.seakim@gmail.com>
2018-07-21 03:38:48 -0400

    fixed structure & added animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,41 @@
-$(document).ready(function () {
+// $(window).load(function() {
 
+$(document).ready( function () {
+    $('.welcome-txt h3').hide();
+    $('.background_source').hide();  
+    $('.welcome-txt h1').addClass("animated zoomInUp");
+    $('#home').addClass("animated rotateIn")
+    setTimeout(() => {
+        $('.welcome-txt h3').fadeIn(2000, function() {
+            $('.welcome-txt h3').addClass("animated bounceIn");   
+            $('.background_source').fadeIn(1000, function() {
+                $('.social').removeClass('invisible').addClass("animated jello");
+            });  
+        });
+    }, 1500);
+
+    // it adds and remove the class without animation
+    $('.hover-bounce').hover(
+        function(){ $(this).addClass('animated bounce') },
+        function(){ $(this).removeClass('animated bounce') }
+    );
+
+    $('.about-img-1').viewportChecker({
+        classToRemove: 'invisible',
+        classToAdd: 'animated slideInRight',
+        offset: 100,
+        repeat: false
+    });
+    $('.about-img-2').viewportChecker({
+        classToRemove: 'invisible',
+        classToAdd: 'animated slideInLeft',
+        offset: 100,
+        repeat: false
+    });
+    $('.ex').viewportChecker({
+        classToRemove: 'invisible',
+        classToAdd: 'animated slideInRight',
+        offset: 100,
+        repeat: false
+    })
 });
