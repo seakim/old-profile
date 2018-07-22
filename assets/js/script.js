8889c2a48bc82c654c880838cc37d1f4c22820cf
sk <dev.seakim@gmail.com>
2018-07-22 01:11:54 -0400

    added more logo/animation & made it responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,31 @@
 // $(window).load(function() {
 
+
+
+
 $(document).ready( function () {
     $('.welcome-txt h3').hide();
     $('.background_source').hide();  
     $('.welcome-txt h1').addClass("animated zoomInUp");
-    $('#home').addClass("animated rotateIn")
+    // $('#home').addClass("animated rotateIn")
     setTimeout(() => {
         $('.welcome-txt h3').fadeIn(2000, function() {
-            $('.welcome-txt h3').addClass("animated bounceIn");   
-            $('.background_source').fadeIn(1000, function() {
-                $('.social').removeClass('invisible').addClass("animated jello");
-            });  
+            $('.social').removeClass('invisible').addClass("animated jello");
+            $('.background_source').fadeIn(1000);
         });
     }, 1500);
 
-    // it adds and remove the class without animation
+    // hover animation
     $('.hover-bounce').hover(
         function(){ $(this).addClass('animated bounce') },
         function(){ $(this).removeClass('animated bounce') }
     );
+    $('#home').hover(
+        function(){ $(this).addClass('animated rotateIn') },
+        function(){ $(this).removeClass('animated rotateIn') }
+    );
 
+    // view port checker slide animation
     $('.about-img-1').viewportChecker({
         classToRemove: 'invisible',
         classToAdd: 'animated slideInRight',
@@ -37,5 +43,46 @@ $(document).ready( function () {
         classToAdd: 'animated slideInRight',
         offset: 100,
         repeat: false
-    })
+    });
+    
+    $('.portfolio-area').viewportChecker({
+        classToRemove: 'invisible',
+        classToAdd: 'animated slideInRight',
+        offset: 100,
+        repeat: false
+    });
+    $('.contact-area').viewportChecker({
+        classToRemove: 'invisible',
+        classToAdd: 'animated slideInLeft',
+        offset: 100,
+        repeat: false
+    });
+    
+    // view port checker logo animation
+    $('.html-img').viewportChecker({
+        classToAdd: 'animated fadeInLeft',
+        offset: 100,
+        repeat: false
+    });
+    $('.js-img').viewportChecker({
+        classToAdd: 'animated zoomInRight',
+        offset: 100,
+        repeat: false
+    });
+    $('.node-img').viewportChecker({
+        classToAdd: 'animated rollIn',
+        offset: 100,
+        repeat: false
+    });
+    $('.java-img').viewportChecker({
+        classToAdd: 'animated wobble',
+        offset: 100,
+        repeat: false
+    });
+    $('.react-img').viewportChecker({
+        classToAdd: 'animated fadeInRight',
+        offset: 100,
+        repeat: false
+    });
+    
 });
